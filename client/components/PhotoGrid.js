b64b7cdbee9159ca88e5a9c738331f3a5aac0ff1
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-09 18:27:18 -0300

    integration store in view

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render(){
       return (
         <div className="photo-grid">
-           PhotoGrid
+          {this.props.posts.map((post, i) => <Photo />)}
         </div>
       )
     }
--- client/reactagram.js
+++ client/reactagram.js
@@ -4,19 +4,24 @@ import {render} from 'react-dom';
 import css from './styles/style.styl';
 
 //components
+import App from './components/App';
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 //router deps
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
