31b31c6d72b9366870e6de064122dee6cf63e2d5
Eduardo Luz <luz.eduardo@gmail.com>
2016-09-17 17:16:41 -0300

    heroku env port conf

--- devServer.js
+++ devServer.js
@@ -17,11 +17,9 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(7770, 'localhost', function(err) {
+app.listen(process.env.PORT || 7770, 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
   }
-
-  console.log('Listening at http://localhost:7770');
 });
